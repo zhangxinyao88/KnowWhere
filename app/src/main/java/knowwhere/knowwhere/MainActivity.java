e2aefb3697c4291423319a984f13434dec06dca6
yuyifan <yuyifan@umd.edu>
2019-01-31 21:12:08 -0500

    update design

--- app/src/main/java/knowwhere/knowwhere/MainActivity.java
+++ app/src/main/java/knowwhere/knowwhere/MainActivity.java
@@ -1,13 +1,11 @@
 package knowwhere.knowwhere;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,41 +13,43 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final float MIN_ACCURACY = 5.0f;
-    private static final long ONE_MIN = 1000 * 60;
+    private static final float MIN_DISTANCE = 10.0f;
+    private static final long TWO_MIN = 1000 * 60 * 2;
+    private static final long POLLING_FREQ = 1000 * 10;
 
-    private static String TAG = "KnowWhere";
-
-    private float mMinDistance = 10000.0f;
-
-    private long mMinTime = 5000;
-    private Location mMarkedLocation;
     private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
+
+    private final String TAG = "KnowWhere";
+
+    private Location mBestReading;
+    private Location mMarkedLocation;
+    private boolean mIsRequestingUpdates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if ((mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE)) == null) {
-            Log.i(TAG, "Fail to get location service");
+        if (null == (mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE)))
             finish();
-        }
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 0);
 
         final Button markButton = findViewById(R.id.markButton);
         markButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Log.i(TAG, "Marking current location");
-                getLocationUpdates();
-                if (mMarkedLocation != null) {
-                    Log.i(TAG, "Location marked");
-                    Toast.makeText(MainActivity.this, "Location Marked", Toast.LENGTH_LONG).show();
+                Location location = getBestLastKnownLocation();
+                if (location != null) {
+                    mMarkedLocation = location;
+                    Toast.makeText(MainActivity.this, "Current Location Marked", Toast.LENGTH_LONG).show();
                 } else {
-                    Log.i(TAG, "Location error");
-                    Toast.makeText(MainActivity.this, "Failed to Mark Location", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Failed to Mark Current Location", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -57,36 +57,49 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         final Button trackButton = findViewById(R.id.trackButton);
         trackButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //MainActivity.this.latestLocation =
-                Log.i(TAG, "track");
+            public void onClick(View v) {
+                if (mMarkedLocation == null) {
+                    Toast.makeText(MainActivity.this, "No Marked Location", Toast.LENGTH_LONG).show();
+                } else {
+                    mLocationListener = getLocationListener();
+                    installLocationListeners();
+                }
             }
         });
     }
 
-    private void getLocationUpdates() {
-        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getApplicationContext(),
-                "android.permission.ACCESS_FINE_LOCATION") != PackageManager.PERMISSION_GRANTED
-                || ContextCompat.checkSelfPermission(getApplicationContext(),
-                "android.permission.ACCESS_COARSE_LOCATION") != PackageManager.PERMISSION_GRANTED) {
-            Log.i(TAG, "Request permission");
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[] {"android.permission.ACCESS_FINE_LOCATION",
-                            "android.permission.ACCESS_COARSE_LOCATION"}, 0);
-        } else {
-            Log.i(TAG, "Start reading location");
-            if (null != mLocationManager.getProvider(LocationManager.NETWORK_PROVIDER))
-                mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, mMinTime, mMinDistance,
-                        this);
-            if (null != mLocationManager.getProvider(LocationManager.GPS_PROVIDER))
-                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, mMinTime, mMinDistance,
-                        this);
-            Location bestResult = null;
-            float bestAccuracy = Float.MAX_VALUE;
-            long bestAge = Long.MIN_VALUE;
-            for (String provider : new String[] {LocationManager.NETWORK_PROVIDER, LocationManager.GPS_PROVIDER}) {
+    private void installLocationListeners() {
+        if (null == mBestReading || mBestReading.getAccuracy() > MIN_ACCURACY || mBestReading.getTime() < System.currentTimeMillis() - TWO_MIN) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            } else {
+                continueInstallLocationListeners();
+            }
+        }
+    }
+
+    private void continueInstallLocationListeners() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (null != mLocationManager.getProvider(LocationManager.NETWORK_PROVIDER)) {
+                Log.i(TAG, "Network location updates requested");
+                mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, POLLING_FREQ, MIN_DISTANCE, mLocationListener);
+                mIsRequestingUpdates = true;
+            }
+            if (null != mLocationManager.getProvider(LocationManager.GPS_PROVIDER)) {
+                Log.i(TAG, "GPS location updates requested");
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, POLLING_FREQ, MIN_DISTANCE, mLocationListener);
+                mIsRequestingUpdates = true;
+            }
+        }
+    }
+
+    private Location getBestLastKnownLocation() {
+        Location bestResult = null;
+        float bestAccuracy = Float.MAX_VALUE;
+        long bestAge = Long.MIN_VALUE;
+        for (String provider : mLocationManager.getAllProviders()) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 Location location = mLocationManager.getLastKnownLocation(provider);
-                Log.i(TAG, provider);
                 if (location != null) {
                     float accuracy = location.getAccuracy();
                     long time = location.getTime();
@@ -97,33 +110,49 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                     }
                 }
             }
-            if (bestAccuracy > MIN_ACCURACY || (System.currentTimeMillis() - bestAge) > ONE_MIN) {
-                mMarkedLocation = null;
-            } else {
-                mMarkedLocation = bestResult;
-            }
         }
-
+        if (bestAccuracy > MIN_ACCURACY || System.currentTimeMillis() - bestAge > TWO_MIN) {
+            return null;
+        } else {
+            return bestResult;
+        }
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
+    private LocationListener getLocationListener() {
+        return new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i(TAG, "New location received");
+                if (null == mBestReading || location.getAccuracy() <= mBestReading.getAccuracy()) {
+                    mBestReading = location;
+                    updateDisplay(location);
+                    if (mBestReading.getAccuracy() < MIN_ACCURACY) {
+                        mLocationManager.removeUpdates(mLocationListener);
+                        Log.i(TAG, "stop updating location");
+                        mIsRequestingUpdates = false;
+                    }
+                }
+            }
 
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
+    private void updateDisplay(Location location) {
+        //TODO: implement here
     }
 
 }
