f0f057a01964e36a9cdc0cfaac245097da95120e
yuyifan <yuyifan@umd.edu>
2019-02-02 11:43:37 -0500

    update main

--- app/src/main/java/knowwhere/knowwhere/MainActivity.java
+++ app/src/main/java/knowwhere/knowwhere/MainActivity.java
@@ -4,18 +4,25 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final float MIN_ACCURACY = 5.0f;
     private static final float MIN_DISTANCE = 10.0f;
@@ -35,10 +42,20 @@ public class MainActivity extends AppCompatActivity {
     private final String TAG = "KnowWhere";
 
     private Location mMarkedLocation;
+    private Location mCurrentLocation;
     private boolean mIsRequestingUpdates;
 
-    private CompassFragment mCompassFragment;
-    private android.app.FragmentManager mFragmentManager;
+    //private CompassFragment mCompassFragment;
+    //private android.app.FragmentManager mFragmentManager;
+
+    private RelativeLayout mFrame;
+    private CompassArrowView mCompassArrow;
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
+    private Sensor magnetometer;
+    private float[] mGravity = null;
+    private float[] mGeomagnetic = null;
+    private double mRotationInDegrees;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +68,24 @@ public class MainActivity extends AppCompatActivity {
         mStopButton.setVisibility(View.INVISIBLE);
 
         // Get a reference to the Fragment
-        mFragmentManager = getFragmentManager();
-        mCompassFragment = (CompassFragment) mFragmentManager.findFragmentById(R.id.compass);
+        //mFragmentManager = getFragmentManager();
+        //mCompassFragment = (CompassFragment) mFragmentManager.findFragmentById(R.id.compass);
 
         if (null == (mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE)))
             finish();
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOC_PERM_ONCREATE);
+
+        mFrame = findViewById(R.id.frame);
+        mCompassArrow = new CompassArrowView(getApplicationContext());
+        mFrame.addView(mCompassArrow);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (null != mSensorManager) {
+            accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        }
+        if (null == accelerometer || null == magnetometer)
+            finish();
 
         mMarkButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                 mLocationListener = getLocationListener();
                 installLocationListeners();
 
-                Log.i(TAG, "start compass");
+                /*Log.i(TAG, "start compass");
                 Intent intent = new Intent(MainActivity.this, CompassActivity.class);
                 Location location = getBestLastKnownLocation();
                 if (mMarkedLocation == null || location == null) {
@@ -95,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("current", location);
                 Log.i(TAG, "put extras");
                 startActivity(intent);
-                Log.i(TAG, "here");
+                Log.i(TAG, "here");*/
             }
         });
 
@@ -167,8 +195,11 @@ public class MainActivity extends AppCompatActivity {
             public void onLocationChanged(Location location) {
                 Log.i(TAG, "New location received");
                 if (null == mMarkedLocation || location.getAccuracy() <= mMarkedLocation.getAccuracy()) {
-                    mMarkedLocation = location;
-                    updateDisplay(location);
+                    mCurrentLocation = location;
+
+                    //updateDisplay(location);
+                    mCompassArrow.invalidate();
+
                     if (location.distanceTo(mMarkedLocation) < TARGET_DISTANCE) {
                         Toast.makeText(MainActivity.this, "You are almost there!", Toast.LENGTH_LONG).show();
                     }
@@ -198,15 +229,72 @@ public class MainActivity extends AppCompatActivity {
         if (mIsRequestingUpdates) {
             installLocationListeners();
         }
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        /*if (mIsRequestingUpdates) {
+        if (mIsRequestingUpdates) {
             mLocationManager.removeUpdates(mLocationListener);
             Log.i(TAG, "Removing location updates in onPause()");
-        }*/
+        }
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //if (mCurrentLocation != null && mMarkedLocation != null) {
+            final float alpha = 0.97f;
+
+            Log.i(TAG, "onSensorChanged");
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                //mGravity[0] = alpha * mGravity[0] + (1 - alpha) * event.values[0];
+                //mGravity[1] = alpha * mGravity[1] + (1 - alpha) * event.values[1];
+                //mGravity[2] = alpha * mGravity[2] + (1 - alpha) * event.values[2];
+                mGravity = new float[3];
+                System.arraycopy(event.values, 0, mGravity, 0, 3);
+            } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                //mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha) * event.values[0];
+                //mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha) * event.values[1];
+                //mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha) * event.values[2];
+                mGeomagnetic = new float[3];
+                System.arraycopy(event.values, 0, mGeomagnetic, 0, 3);
+            }
+            if (mGravity != null && mGeomagnetic != null) {
+                float rotationMatrix[] = new float[9];
+                if (SensorManager.getRotationMatrix(rotationMatrix, null, mGravity, mGeomagnetic)) {
+                    float orientationMatrix[] = new float[3];
+                    SensorManager.getOrientation(rotationMatrix, orientationMatrix);
+                    mRotationInDegrees = Math.toDegrees(orientationMatrix[0]);
+
+                    mRotationInDegrees = (mRotationInDegrees + 360) % 360;
+                    //mRotationInDegrees -= bearing(mCurrentLocation, mMarkedLocation);
+
+                    Log.i(TAG, "updating canvas");
+                    mCompassArrow.invalidate();
+                    Log.i(TAG, "canvas updated");
+                    mGravity = mGeomagnetic = null;
+                }
+            }
+        //}
+    }
+
+    protected double bearing(Location l1, Location l2) {
+        double long1 = l1.getLongitude();
+        double long2 = l2.getLongitude();
+        double lat1 = Math.toRadians(l1.getLatitude());
+        double lat2 = Math.toRadians(l2.getLatitude());
+        double longDif = Math.toRadians(long2 - long1);
+        double y = Math.sin(longDif) * Math.cos(lat2);
+        double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(longDif);
+        return (Math.toDegrees(Math.atan2(y, x)) + 360) % 360;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     @Override
@@ -222,6 +310,44 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateDisplay(Location location) {
         //TODO: implement here
+    }
+
+    public class CompassArrowView extends View {
+
+        int mParentWidth;
+        int mParentHeight;
+        int mBitmapWidth;
+        int mParentCenterX;
+        int mParentCenterY;
+        int mViewTopX;
+        int mViewLeftY;
+        Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.arrow);
+
+        public CompassArrowView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            mParentWidth = mFrame.getWidth();
+            mParentHeight = mFrame.getHeight();
+            int size = Math.round(Math.min(mParentHeight, mParentWidth) / 1.33f);
+            mBitmap = Bitmap.createScaledBitmap(mBitmap, size, size, false);
+            mBitmapWidth = mBitmap.getWidth();
+            mParentCenterX = mParentWidth / 2;
+            mParentCenterY = mParentHeight / 2;
+            mViewLeftY = mParentCenterX - mBitmapWidth / 2;
+            mViewTopX = mParentCenterY - mBitmapWidth / 2;
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.save();
+            canvas.rotate((float) -mRotationInDegrees, mParentCenterX, mParentCenterY);
+            canvas.drawBitmap(mBitmap, mViewLeftY, mViewTopX, null);
+            canvas.restore();
+        }
+
     }
 
 }
